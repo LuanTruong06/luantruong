6e9b4cd58b32f4305b8fde339a98a226bd27262b
Luan Truong <74796589+LuanTruong06@users.noreply.github.com>
2020-11-27 15:37:38 -0500

    Add files via upload
    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+sr.reveal('.nav-links',{})
+sr.reveal('.pic',{delay: 200});
+sr.reveal('.rocket',{delay: 400});
+
+sr.reveal('.rocket1',{});
+sr.reveal('.rocket2',{});
+sr.reveal('.rocket3',{});
+sr.reveal('.rocket4',{});
+sr.reveal('.rocket5',{});
+sr.reveal('.aboutMe',{delay:100});
+sr.reveal('.Skills',{delay:100});
+sr.reveal('.Education',{delay:100});
+sr.reveal('.Project',{delay:100});
+sr.reveal('.iss',{delay:100});
